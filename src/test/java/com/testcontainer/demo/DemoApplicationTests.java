08c76fe80f74b83f13164d8ce62c7de437604c68
kirithekaDhanabalan <kirithi.balan@gmail.com>
2025-05-21 22:53:09 +0530

    container test for sql test working connectivity

--- src/test/java/com/testcontainer/demo/DemoApplicationTests.java
+++ src/test/java/com/testcontainer/demo/DemoApplicationTests.java
@@ -1,15 +1,25 @@
 package com.testcontainer.demo;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MySQLContainer;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
+    @Test
+    void testMySQLContainer() {
+        try (MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")) {
+            mysql.start();
+            assertTrue(mysql.isRunning());
+        }
+    }
 }
